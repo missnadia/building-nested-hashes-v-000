c7a7f54131cc16c164664701daf20d1bcb302302
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-13 20:55:58 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -20,7 +20,7 @@ def fourth_challenge
       :heroine_friends => [
         {:name => "Steven",
          :age => "30",
-         :attitude => "confused"}
+         :attitude => "confused"},
         {:name => "Nurse",
          :age => "44",
          :attitude => "worried"}
